b3cdff7cd847ad66f0fbece92bcbddcdea56034a
Ed Whyatt <lizardxyz@googlemail.com>
2022-10-14 14:14:25 +0000

    Add runtime txt file

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,8 +16,6 @@ import dj_database_url
 if os.path.exists("env.py"):
     import env
 
-DEVELOPMENT = os.environ.get("DEVELOPMENT")
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -29,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = DEVELOPMENT
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
